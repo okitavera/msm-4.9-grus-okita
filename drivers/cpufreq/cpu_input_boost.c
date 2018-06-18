4850ec3b70eebf556619f53516deaeec1b4ed8eb
joshuous <joshuous@gmail.com>
2018-06-18 15:47:01 +0200

    cpu_input_boost: Reset Dynamic SchedTune Boost only if it is currently active
    We need to ensure that every do_stune_boost() that is called is met by
    the same number of reset_stune_boost() so that the stune_boost_count is
    managed correctly.
    
    Signed-off-by: joshuous <joshuous@gmail.com>
    [nc: Adapt from cpu-boost.c to cpu_input_boost.c]
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -17,6 +17,7 @@ static unsigned int input_boost_freq_hp = CONFIG_INPUT_BOOST_FREQ_PERF;
 static unsigned short input_boost_duration = CONFIG_INPUT_BOOST_DURATION_MS;
 
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
+static bool stune_boost_active;
 static unsigned short dynamic_stune_boost;
 module_param(dynamic_stune_boost, short, 0644);
 #endif
@@ -147,7 +148,8 @@ static void input_boost_worker(struct work_struct *work)
 	}
 
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
-	do_stune_boost("top-app", dynamic_stune_boost);
+	if (!do_stune_boost("top-app", dynamic_stune_boost))
+		stune_boost_active = true;
 #endif
 	queue_delayed_work(b->wq, &b->input_unboost,
 		msecs_to_jiffies(input_boost_duration));
@@ -160,7 +162,10 @@ static void input_unboost_worker(struct work_struct *work)
 
 	clear_boost_bit(b, INPUT_BOOST);
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
-	reset_stune_boost("top-app");
+	if (stune_boost_active) {
+		reset_stune_boost("top-app");
+		stune_boost_active = false;
+	}
 #endif
 	update_online_cpu_policy();
 }
@@ -175,7 +180,8 @@ static void max_boost_worker(struct work_struct *work)
 	}
 
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
-	do_stune_boost("top-app", dynamic_stune_boost);
+	if (!do_stune_boost("top-app", dynamic_stune_boost))
+		stune_boost_active = true;
 #endif
 	queue_delayed_work(b->wq, &b->max_unboost,
 		msecs_to_jiffies(atomic_read(&b->max_boost_dur)));
@@ -188,7 +194,10 @@ static void max_unboost_worker(struct work_struct *work)
 
 	clear_boost_bit(b, WAKE_BOOST | MAX_BOOST);
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
-	reset_stune_boost("top-app");
+	if (stune_boost_active) {
+		reset_stune_boost("top-app");
+		stune_boost_active = false;
+	}
 #endif
 	update_online_cpu_policy();
 }
@@ -299,7 +308,10 @@ free_handle:
 static void cpu_input_boost_input_disconnect(struct input_handle *handle)
 {
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
-	reset_stune_boost("top-app");
+	if (stune_boost_active) {
+		reset_stune_boost("top-app");
+		stune_boost_active = false;
+	}
 #endif
 	input_close_device(handle);
 	input_unregister_handle(handle);
