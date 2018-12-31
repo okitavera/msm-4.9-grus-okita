88361b0c7241f54eddbdcffe200af2f62b82d280
DevriesL <therkduan@gmail.com>
2019-01-01 00:27:12 +0800

    drm: make OLED great again
    Change-Id: Iae3dcbff083d6a34ae6d8af21415c8726141cd0f
    
    Signed-off-by: DevriesL <therkduan@gmail.com>
    Signed-off-by: Nanda Oktavera <codeharuka.yusa@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -40,6 +40,10 @@
 #include <linux/klapse.h>
 #endif
 
+#ifdef CONFIG_EXPOSURE_ADJUSTMENT
+#include "exposure_adjustment.h"
+#endif
+
 /**
  * topology is currently defined by a set of following 3 values:
  * 1. num of layer mixers
@@ -780,6 +784,10 @@ int dsi_panel_set_backlight(struct dsi_panel *panel, u32 bl_lvl)
 
 	if (panel->type == EXT_BRIDGE)
 		return 0;
+
+#ifdef CONFIG_EXPOSURE_ADJUSTMENT
+	bl_lvl = ea_panel_calc_backlight(bl_lvl);
+#endif
 
 	pr_debug("backlight type:%d lvl:%d\n", bl->type, bl_lvl);
 
