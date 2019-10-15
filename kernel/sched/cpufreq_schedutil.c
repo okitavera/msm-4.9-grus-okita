245fda3ea0b4cfbdc4aa01d7f219790092f6549b
Nanda Oktavera <codeharuka.yusa@gmail.com>
2019-10-15 20:37:08 +0900

    cpufreq: schedutil: properly restore cached ratelimits on governor init
    Signed-off-by: Nanda Oktavera <codeharuka.yusa@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -866,6 +866,9 @@ static void sugov_tunables_restore(struct cpufreq_policy *policy)
 	tunables->hispeed_freq = cached->hispeed_freq;
 	tunables->up_rate_limit_us = cached->up_rate_limit_us;
 	tunables->down_rate_limit_us = cached->down_rate_limit_us;
+	sg_policy->up_rate_delay_ns = cached->up_rate_limit_us;
+	sg_policy->down_rate_delay_ns = cached->down_rate_limit_us;
+	update_min_rate_limit_ns(sg_policy);
 }
 
 static int sugov_init(struct cpufreq_policy *policy)
